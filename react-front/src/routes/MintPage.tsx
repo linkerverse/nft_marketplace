882cca9923b9a0a7f8e9a814a638673b9bef94b6
김재만 <daga4242@gmail.com>
2022-09-28 13:25:19 +0900

    fix: react template delete

--- react-front/src/routes/MintPage.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-
-const MintPage: FC = () => {
-  return <>MintPage</>;
-};
-
-export default MintPage;
--- react-front/src/routes/SellingPage.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-
-const SellingPage: FC = () => {
-  return <>SellingPage</>;
-};
-
-export default SellingPage;
